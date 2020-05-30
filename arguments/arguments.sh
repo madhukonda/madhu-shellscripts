f2e1a4bb0a77bb249eb475d5e06b96d79d50dec8
madhu <madhukonda63@gmail.com>
2020-05-30 13:32:10 +0530

    added arguments file

---
+++ arguments/arguments.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo $0  #this will display file name
+echo $1  # this will display the first argument
+echo $2   # this will display the second argument on console
+echo $@   # this will displays all the arguments on console
+echo $*    # this will displays all the arguments on console
+echo $#    # this will displays the number of arguments passed on the console
+echo $$    # this will displays PID of the current program
+
